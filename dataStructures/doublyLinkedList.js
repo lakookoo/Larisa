195b44dd51169c5b6bbb2d960eaeb980364d472a
Larisa <kosareva.larisa@gmail.com>
2024-04-10 16:56:12 +0200

    dll unshift(add new head)

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -43,20 +43,34 @@ class DoublyLinkedList {
         return poppedNode;
     }
 
-    shift(){
-        if(!this.head === 0) return undefined;
+    shift() {
+        if (!this.head === 0) return undefined;
         let oldHead = this.head;
         if (this.length === 1) {
             this.head = null;
-            this.tail = null; 
+            this.tail = null;
         } else {
             this.head = oldHead.next;
-        this.head.prev = null;
-        oldHead.next = null;
+            this.head.prev = null;
+            oldHead.next = null;
         }
-        
+
         this.length--;
-    
+
         return oldHead;
+    }
+
+    unshift(val){
+        let node = new Node(val);
+        if(!this.head){
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            this.head.prev = node
+            node.next = this.head;
+            this.head = node; 
+        }
+        this.length++;
+        return this;  
     }
 }
