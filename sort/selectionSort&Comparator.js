b579be96868c907e52ff8cec11ba1d3c3fefc00a
Larisa <kosareva.larisa@gmail.com>
2024-06-17 12:41:26 +0200

    selection sort with comparator

--- sort/selectionSort&Comparator.js
+++ sort/selectionSort&Comparator.js
@@ -1,3 +1,21 @@
 function selectionSort( arr, comparator){
-    
+
+    if (typeof comparator !== 'function'){
+        comparator = function(a, b){
+            return a - b
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++){
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++){
+            if(comparator(arr[j], arr[minIndex]) < 0) {
+                minIndex = j
+            }
+        }
+        if (i !== minIndex) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+        }
+    }
+    return arr;
 }
